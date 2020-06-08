2edf77c3e5c13e249c623a675798638c6c1f7a5b
Johannes B <johannes.bock@live.de>
2020-06-08 12:36:00 +0200

    Projektdateien hinzufügen.

---
+++ Klausur_T2_v1_SS20/Körper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Klausur_T2_v1_SS20
+{
+    abstract class Körper
+    {
+        public double x, y;
+        protected double ay, vy=0;
+        Polygon umriss;
+        static protected Random rnd = new Random();
+
+        public Körper(double x, double Radius)
+        {
+            this.x = x;
+            y = 0;
+            ay = 1000;
+
+            umriss = new Polygon();
+            for (int i = 0; i < 20; i++)
+            {
+                double r = Radius / 2.0 * rnd.NextDouble() + Radius;
+                double winkel = 2 * Math.PI / 20 * i;
+                umriss.Points.Add(new System.Windows.Point(r * Math.Cos(winkel), r * Math.Sin(winkel)));
+            }
+            umriss.Fill = Brushes.Gray;
+        }
+
+        public bool Bewege(Canvas ZF, TimeSpan time)
+        {
+            bool rausgeflogen = false;
+            vy += ay * time.TotalSeconds;
+            y += vy*time.TotalSeconds;
+            if (y > ZF.ActualHeight) rausgeflogen = true;
+            if (y < 0) rausgeflogen = true;
+            return rausgeflogen;
+        }
+
+        public void Zeichne(Canvas ZF)
+        {
+            ZF.Children.Add(umriss);
+            Canvas.SetLeft(umriss, x);
+            Canvas.SetTop(umriss, y);
+        }
+
+        public  void ÄndereFarbe()
+        {
+            if (umriss.Fill == Brushes.Gray) umriss.Fill = Brushes.Brown;
+            else umriss.Fill = Brushes.Gray;
+        }
+    }
+
+    class Kiesel : Körper
+    {
+       
+        public Kiesel(Canvas ZF)
+            : base(rnd.NextDouble()*ZF.ActualWidth, 6)
+        {
+            
+        }
+
+    }
+
+    class Stein : Körper
+    {
+        public Stein(Canvas ZF)
+            : base(rnd.NextDouble() * ZF.ActualWidth, 12)
+        {
+            
+        }
+    }
+
+    class Fels : Körper
+    {
+        
+        public Fels(Canvas ZF)
+            : base(rnd.NextDouble() * ZF.ActualWidth, 24)
+        {
+            
+        }
+    }
+}
---
+++ Klausur_T2_v1_SS20/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Klausur_T2_v1_SS20
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Körper> AlleKörper = new List<Körper>();
+        List<Körper> zuLöschendeKörper = new List<Körper>();
+
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(17);
+            timer.Tick += Animiere;
+        }
+
+        private void Animiere(object sender, EventArgs e)
+        {
+            Zeichenfläche.Children.Clear();
+            foreach (Körper k in AlleKörper)
+            {
+                if(k.Bewege(Zeichenfläche, timer.Interval))
+                {
+                    zuLöschendeKörper.Add(k);
+                }
+                k.Zeichne(Zeichenfläche);
+            }
+            AlleKörper.RemoveAll(x => zuLöschendeKörper.Contains(x));
+        }
+
+        private void BTN_Start_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            BTN_Start.IsEnabled = false;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                switch (e.Key)
+                {
+                    case Key.K:
+                        AlleKörper.Add(new Kiesel(Zeichenfläche));
+                        break;
+                    case Key.S:
+                        AlleKörper.Add(new Stein(Zeichenfläche));
+                        break;
+                    case Key.F:
+                        AlleKörper.Add(new Fels(Zeichenfläche));
+                        break;
+                    case Key.Space:
+                        AlleKörper.ForEach(x => x.ÄndereFarbe());
+                        break;
+                }
+            }
+        }
+    }
+}
